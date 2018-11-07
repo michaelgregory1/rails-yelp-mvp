ebf84dbb84bc2c0c461a5280ffa98b7d57894454
michael gregory <michaelgregory8976@gmail.com>
2018-11-07 20:11:33 +0000

    done

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,48 @@
+class RestaurantsController < ApplicationController
+
+before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurants_path
+    else
+      render :new
+    end
+  end
+
+  def update
+
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def destroy
+
+  end
+
+
+  private
+
+    def set_restaurant
+      @restaurant = Restaurant.find(params[:id])
+    end
+
+    def restaurant_params
+      params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+    end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,68 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+# Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '17 Boundary St, London E2 7JE',
+    phone_number: '7843222',
+    category:     'chinese'
+
+  },
+  {
+    name:         'Wagamas',
+    address:      '2 Mulberry St, London E2 7JE',
+    phone_number: '4356748569',
+    category:     'italian'
+  },
+  {
+    name:         'Nandos',
+    address:      '7 Zohan St, London E2 7JE',
+    phone_number: '546738',
+    category:     'french'
+  },
+  {
+    name:         'GBK',
+    address:      '21 Magnum St, London E2 7JE',
+    phone_number: '6574839',
+    category:     'belgian'
+  },
+  {
+    name:         'Pizza Express',
+    address:      '56 Zoolander St, London E2 7JE',
+    phone_number: '6547839',
+    category:     'japanese'
+  }
+]
+# Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
+
+reviews_attributes = [
+{
+  content:       'was amazing, would go back again',
+  rating:         3,
+  restaurant_id:  16
+},
+{
+  content:       'was amazing, would go back again',
+  rating:         5,
+  restaurant_id:  16
+},
+{
+  content:       'was amazing, would go back again',
+  rating:         4,
+  restaurant_id:  16
+},
+{
+  content:       'was amazing, would go back again',
+  rating:         5,
+  restaurant_id:  16
+},
+{
+  content:       'was amazing, would go back again',
+  rating:         4,
+  restaurant_id:  16
+}
+]
+Review.create!(reviews_attributes)
